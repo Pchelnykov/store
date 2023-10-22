99aeaf1e8a4917ce6bef3b34622ee7cd2445ee56
pchelnykov <pchelnykov@pop-os.localdomain>
2023-10-22 18:46:03 +0300

    created singup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,8 @@ import Helmet from "../components/Helmet";
 import { Link } from "react-router-dom";
 import "../styles/login.css";
 const Login = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
   return (
     <Helmet title="Login">
       <section>
@@ -13,12 +15,24 @@ const Login = () => {
               <h3 className="fs-4 fw-bold">Login</h3>
               <Form className="auth__form">
                 <FormGroup className="form__group">
-                  <input type="email" placeholder="Enter your email" />
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </FormGroup>
                 <FormGroup className="form__group">
-                  <input type="password" placeholder="Enter your password" />
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </FormGroup>
-                <button className="buy__btn auth__btn">Sing in</button>
+                <button type="submit" className="buy__btn auth__btn">
+                  Sing in
+                </button>
                 <p className="mt-2">
                   Don't have an account?
                   <Link to="/singup">Create an account</Link>
--- src/pages/Singup.jsx
+++ src/pages/Singup.jsx
@@ -1,7 +1,49 @@
 import React from "react";
-
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import Helmet from "../components/Helmet";
+import { Link } from "react-router-dom";
+import "../styles/login.css";
 const Singup = () => {
-  return <div>Singup</div>;
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  return (
+    <Helmet title="Singup">
+      <section>
+        <Container>
+          <Row>
+            <Col className="m-auto text-center" lg="6">
+              <h3 className="fs-4 fw-bold">Singup</h3>
+              <Form className="auth__form">
+                <FormGroup className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+                <button type="submit" className="buy__btn auth__btn">
+                  Singup
+                </button>
+                <p className="mt-2">
+                  Already have an account?
+                  <Link to="/login">Login</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Singup;
